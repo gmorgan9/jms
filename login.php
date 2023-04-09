e811bddf78ad815f8d87209fd861fe3c6aa276c2
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-04-09 02:00:12 -0600

    update

--- login.php
+++ login.php
@@ -64,7 +64,7 @@ if(mysqli_num_rows($result) > 0){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/pre.css?v=2.6">
+    <link rel="stylesheet" href="assets/css/pre.css?v=2.6">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
     <!-- Bootstrap Icons -->
