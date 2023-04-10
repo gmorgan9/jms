4178f1eecf61bb7c8eb890268c61e338a2c5494d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-04-09 21:45:37 -0600

    update

--- index.php
+++ index.php
@@ -140,8 +140,10 @@ if (!isLoggedIN()) {
                     <div class="card-body">
                         <ul class="list-group">
                             <li class="list-group-item">
-                                <p class="float-start">beginning</p>
-                                <p class="float-end">backend</p> 
+                                <div>
+                                    <p class="float-start">beginning</p>
+                                    <p class="float-end">backend</p> 
+                                </div>
                                 <br>
                                 <p class="text-muted" style="font-size: 11px;">American Airlines</p>
                             </li>
