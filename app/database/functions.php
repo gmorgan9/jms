4ffb7479cea627f5787a1602cfd3dd800014f720
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-04-10 11:48:44 -0600

    update

--- app/database/functions.php
+++ app/database/functions.php
@@ -42,38 +42,53 @@ if(isset($_POST['add-quick'])){
 // END ADD JOB
 
 
-// ADD JOB
-if (isset($_POST['add-full'])) {
-    $idno  = rand(1000000, 9999999);
-	$watchlist = isset($_POST['watchlist']) ? 1 : 0;
-	$interview_set = isset($_POST['interview_set']) ? 1 : 0;
-	if(isset($_POST['job_title'])) { $job_title = mysqli_real_escape_string($conn, $_POST['job_title']); } else { $job_title = ""; }
-	if(isset($_POST['company'])) { $company = mysqli_real_escape_string($conn, $_POST['company']); } else { $company = ""; }
-	if(isset($_POST['location'])) { $location = mysqli_real_escape_string($conn, $_POST['location']); } else { $location = ""; }
-	if(isset($_POST['job_desc'])) { $job_desc = mysqli_real_escape_string($conn, $_POST['job_desc']); } else { $job_desc = ""; }
-	if(isset($_POST['pay'])) { $pay = mysqli_real_escape_string($conn, $_POST['pay']); } else { $pay = ""; }
-	if(isset($_POST['bonus_pay'])) { $bonus_pay = mysqli_real_escape_string($conn, $_POST['bonus_pay']); } else { $bonus_pay = ""; }
-	if(isset($_POST['status'])) { $status = mysqli_real_escape_string($conn, $_POST['status']); } else { $status = ""; }
-	if(isset($_POST['app_link'])) { $app_link = mysqli_real_escape_string($conn, $_POST['app_link']); } else { $app_link = ""; }
-	if(isset($_POST['job_type'])) { $job_type = mysqli_real_escape_string($conn, $_POST['job_type']); } else { $job_type = ""; }
-	if(isset($_POST['contact_name'])) { $contact_name = mysqli_real_escape_string($conn, $_POST['contact_name']); } else { $contact_name = ""; }
-	if(isset($_POST['contact_email'])) { $contact_email = mysqli_real_escape_string($conn, $_POST['contact_email']); } else { $contact_email = ""; }
-	if(isset($_POST['contact_phone'])) { $contact_phone = mysqli_real_escape_string($conn, $_POST['contact_phone']); } else { $contact_phone = ""; }
-	if(isset($_POST['start_date'])) { $start_date = mysqli_real_escape_string($conn, $_POST['start_date']); } else { $start_date = ""; }
-	if(isset($_POST['resume_used'])) { $resume_used = mysqli_real_escape_string($conn, $_POST['resume_used']); } else { $resume_used = ""; }
-	if(isset($_POST['notes'])) { $notes = mysqli_real_escape_string($conn, $_POST['notes']); } else { $notes = ""; }
-    
+// ADD FULL APPLICATION
+	if (isset($_POST['add-full'])) {
+		$idno  = rand(1000000, 9999999);
+		$watchlist = isset($_POST['watchlist']) ? 1 : 0;
+		$interview_set = isset($_POST['interview_set']) ? 1 : 0;
+		if(isset($_POST['job_title'])) { $job_title = mysqli_real_escape_string($conn, $_POST['job_title']); } else { $job_title = ""; }
+		if(isset($_POST['company'])) { $company = mysqli_real_escape_string($conn, $_POST['company']); } else { $company = ""; }
+		if(isset($_POST['location'])) { $location = mysqli_real_escape_string($conn, $_POST['location']); } else { $location = ""; }
+		if(isset($_POST['job_desc'])) { $job_desc = mysqli_real_escape_string($conn, $_POST['job_desc']); } else { $job_desc = ""; }
+		if(isset($_POST['pay'])) { $pay = mysqli_real_escape_string($conn, $_POST['pay']); } else { $pay = ""; }
+		if(isset($_POST['bonus_pay'])) { $bonus_pay = mysqli_real_escape_string($conn, $_POST['bonus_pay']); } else { $bonus_pay = ""; }
+		if(isset($_POST['status'])) { $status = mysqli_real_escape_string($conn, $_POST['status']); } else { $status = ""; }
+		if(isset($_POST['app_link'])) { $app_link = mysqli_real_escape_string($conn, $_POST['app_link']); } else { $app_link = ""; }
+		if(isset($_POST['job_type'])) { $job_type = mysqli_real_escape_string($conn, $_POST['job_type']); } else { $job_type = ""; }
+		if(isset($_POST['contact_name'])) { $contact_name = mysqli_real_escape_string($conn, $_POST['contact_name']); } else { $contact_name = ""; }
+		if(isset($_POST['contact_email'])) { $contact_email = mysqli_real_escape_string($conn, $_POST['contact_email']); } else { $contact_email = ""; }
+		if(isset($_POST['contact_phone'])) { $contact_phone = mysqli_real_escape_string($conn, $_POST['contact_phone']); } else { $contact_phone = ""; }
+		if(isset($_POST['start_date'])) { $start_date = mysqli_real_escape_string($conn, $_POST['start_date']); } else { $start_date = ""; }
+		if(isset($_POST['resume_used'])) { $resume_used = mysqli_real_escape_string($conn, $_POST['resume_used']); } else { $resume_used = ""; }
+		if(isset($_POST['notes'])) { $notes = mysqli_real_escape_string($conn, $_POST['notes']); } else { $notes = ""; }
+		
 
-    $select = "SELECT * FROM applications WHERE idno = '$idno'";
-    $result = mysqli_query($conn, $select);
+		$select = "SELECT * FROM applications WHERE idno = '$idno'";
+		$result = mysqli_query($conn, $select);
 
-    if (mysqli_num_rows($result) > 0) {
-        $error[] = 'Application already exists!';
-    } else {
-        $insert = "INSERT INTO applications (idno, job_title, company, location, job_desc, pay, bonus_pay, status, watchlist, app_link, job_type, contact_name, contact_email, contact_phone, interview_set, start_date, resume_used, notes) 
-        VALUES ('$idno',NULLIF('$job_title',''),NULLIF('$company',''),NULLIF('$location',''),NULLIF('$job_desc',''),NULLIF('$pay',''),NULLIF('$bonus_pay',''),NULLIF('$status',''),'$watchlist',NULLIF('$app_link',''),NULLIF('$job_type',''),NULLIF('$contact_name',''),NULLIF('$contact_email',''),NULLIF('$contact_phone',''),'$interview_set',NULLIF('$start_date',''),NULLIF('$resume_used',''),NULLIF('$notes',''))";
-        mysqli_query($conn, $insert);
-        header('location: /');
-    }
-}
-// END ADD JOB
+		if (mysqli_num_rows($result) > 0) {
+			$error[] = 'Application already exists!';
+		} else {
+			$insert = "INSERT INTO applications (idno, job_title, company, location, job_desc, pay, bonus_pay, status, watchlist, app_link, job_type, contact_name, contact_email, contact_phone, interview_set, start_date, resume_used, notes) 
+			VALUES ('$idno',NULLIF('$job_title',''),NULLIF('$company',''),NULLIF('$location',''),NULLIF('$job_desc',''),NULLIF('$pay',''),NULLIF('$bonus_pay',''),NULLIF('$status',''),'$watchlist',NULLIF('$app_link',''),NULLIF('$job_type',''),NULLIF('$contact_name',''),NULLIF('$contact_email',''),NULLIF('$contact_phone',''),'$interview_set',NULLIF('$start_date',''),NULLIF('$resume_used',''),NULLIF('$notes',''))";
+			mysqli_query($conn, $insert);
+			header('location: /');
+		}
+	}
+// END ADD FULL APPLICATION
+
+// DELETE
+	if(isset($_GET['appid'])) {
+		$id = $_GET['appid'];
+
+		$sql = "DELETE FROM applications WHERE appid=$id";
+		$result = mysqli_query($con, $sql);
+		if($result) {
+			// echo "Deleted Successfully";
+			// header('location: /'); // returns back to same page
+		} else {
+			die(mysqli_error($con));
+		}
+	}
+// END DELETE
