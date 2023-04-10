b7fea057c06ffef923a048506c4e1810116cedcb
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-04-10 12:41:30 -0600

    update

--- update-app.php
+++ update-app.php
@@ -89,46 +89,46 @@ if (!isLoggedIN()) {
 
     <form method="POST" action="">
 
-        <!-- <div class="row mb-3">
+        <div class="row mb-3">
             <div class="col">
                 <label for="appid" class="form-label">App ID</label>
                 <input type="text" class="form-control" id="appid" name="appid" value="<?php echo $appid; ?>">
             </div>
-        </div> -->
+        </div>
 
         <div class="row mb-3">
             <div class="col">
                 <label for="job_title" class="form-label">Job Title</label>
-                <input type="text" class="form-control" id="job_title" name="job_title">
+                <input type="text" class="form-control" id="job_title" name="job_title" value="<?php echo $job_title; ?>">
             </div>
         </div>
 
         <div class="row mb-3">
             <div class="col">
                 <label for="company" class="form-label">Company</label>
-                <input type="text" class="form-control" id="company" name="company">
+                <input type="text" class="form-control" id="company" name="company" value="<?php echo $comapny; ?>">
             </div>
             <div class="col">
                 <label for="location" class="form-label">Location</label>
-                <input type="text" class="form-control" id="location" name="location">
+                <input type="text" class="form-control" id="location" name="location" value="<?php echo $location; ?>">
             </div>
         </div>
     
         <div class="row mb-3">
             <div class="col">
                 <label class="form-label" for="job_desc">Job Description</label>
-                <textarea class="form-control" name="job_desc" rows="5"></textarea>
+                <textarea class="form-control" name="job_desc" rows="5"><?php echo $job_desc; ?></textarea>
             </div>
         </div>
 
         <div class="row mb-3">
             <div class="col">
                 <label for="pay" class="form-label">Pay</label>
-                <input type="text" class="form-control" id="pay" name="pay">
+                <input type="text" class="form-control" id="pay" name="pay" value="<?php echo $pay; ?>">
             </div>
             <div class="col">
                 <label for="bonus_pay" class="form-label">Bonus Pay  <span class="text-muted" style="font-size: 10px;">Optional</span></label>
-                <input type="text" class="form-control" id="bonus_pay" name="bonus_pay">
+                <input type="text" class="form-control" id="bonus_pay" name="bonus_pay" value="<?php echo $bonus_pay; ?>">
             </div>
         </div>
 
@@ -136,6 +136,7 @@ if (!isLoggedIN()) {
             <div class="col">
                 <label class="form-label" for="status">Status</label>
                 <select class="form-control" name="status">
+                    <option value="<?php echo $status; ?>"><?php echo $status; ?></option>
                     <option value="applied">Applied</option>
                     <option value="interviewed">Interviewed</option>
                     <option value="offered">Offered</option>
@@ -145,6 +146,7 @@ if (!isLoggedIN()) {
             <div class="col">
                 <label class="form-label" for="job_type">Job Type</label>
                 <select class="form-control" name="job_type">
+                    <option value="<?php echo $job_type; ?>"><?php echo $job_type; ?></option>
                     <option value="applied">Full Time</option>
                     <option value="interviewed">Part Time</option>
                     <option value="offered">Contract</option>
@@ -157,7 +159,7 @@ if (!isLoggedIN()) {
         <div class="row mb-3">
             <div class="col">
                 <label for="app_link" class="form-label">Application Link</label>
-                <input type="text" class="form-control" id="app_link" name="app_link">
+                <input type="text" class="form-control" id="app_link" name="app_link" value="<?php echo $app_link; ?>">
             </div>
         </div>
 
@@ -166,18 +168,18 @@ if (!isLoggedIN()) {
         <div class="row mb-3">
             <div class="col">
                 <label for="contact_name" class="form-label">Contact Name</label>
-                <input type="text" class="form-control" id="contact_name" name="contact_name">
+                <input type="text" class="form-control" id="contact_name" name="contact_name" value="<?php echo $contact_name; ?>">
             </div>
             <div class="col">
                 <label for="contact_phone" class="form-label">Contact Phone</label>
-                <input type="text" class="form-control" id="contact_phone" name="contact_phone">
+                <input type="text" class="form-control" id="contact_phone" name="contact_phone" value="<?php echo $contact_phone; ?>">
             </div>
         </div>
 
         <div class="row mb-3">
             <div class="col">
                 <label for="contact_email" class="form-label">Contact Email</label>
-                <input type="text" class="form-control" id="contact_email" name="contact_email">
+                <input type="text" class="form-control" id="contact_email" name="contact_email" value="<?php echo $contact_email; ?>">
             </div>
         </div>
 
@@ -186,28 +188,36 @@ if (!isLoggedIN()) {
         <div class="row mb-3">
             <div class="col">
                 <label for="start_date" class="form-label">Start Date</label>
-                <input type="date" class="form-control" id="start_date" name="start_date">
+                <input type="date" class="form-control" id="start_date" name="start_date" value="<?php echo $start_date; ?>">
             </div>
             <div class="col">
                 <label for="resume_used" class="form-label">Resume Used</label>
-                <input type="text" class="form-control" id="resume_used" name="resume_used">
+                <input type="text" class="form-control" id="resume_used" name="resume_used" value="<?php echo $resume_used; ?>">
             </div>
         </div>
 
         <div class="row mb-3">
             <div class="col">
                 <label class="form-label" for="notes">Notes</label>
-                <textarea class="form-control" name="notes" rows="5"></textarea>
+                <textarea class="form-control" name="notes" rows="5"><?php echo $notes; ?></textarea>
             </div>
         </div>
 
         <div class="row mb-3 ps-3">
             <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="watchlist" name="watchlist" value="1">
+                <!-- <?php //if($watchlist == 1) { ?>
+                    <input type="checkbox" class="form-check-input" id="watchlist" name="watchlist" value="1" checked>
+                    <label class="form-check-label" for="watchlist">Add to Watchlist</label>
+                <?php //} else { ?>
+                    <input type="checkbox" class="form-check-input" id="watchlist" name="watchlist" value="0">
+                    <label class="form-check-label" for="watchlist">Add to Watchlist</label>
+                <?php //} ?> -->
+                <input type="checkbox" class="form-check-input" id="watchlist" name="watchlist" value="<?php echo $watchlist; ?>">
                 <label class="form-check-label" for="watchlist">Add to Watchlist</label>
+
             </div>
             <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="interview_set" name="interview_set" value="1">
+                <input type="checkbox" class="form-check-input" id="interview_set" name="interview_set" value="<?php echo $interview_set; ?>">
                 <label class="form-check-label" for="interview_set">Interview Set</label>
             </div>
         </div>
