695d8b23cee255eddd2b25c04140808b6639dc5b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-04-10 14:59:15 -0600

    update

--- open-app.php
+++ open-app.php
@@ -195,9 +195,13 @@ if (!isLoggedIN()) {
                                     <div class="ms-3 me-3">
                                        <p class="float-start fw-bold">Status</p> 
                                        <?php if($cap['status'] == 'Applied'){ ?>
-                                            <p><span class="float-end"><i style="font-size: 12px;" class="bi bi-circle-fill text-primary"></i> &nbsp; <?php echo $cap['status']; ?></span></p>
-                                        <?php } else { ?>
-                                            <p><span class="float-end"><?php echo $cap['status']; ?></span></p>
+                                            <p><span class="float-end"><i style="font-size: 12px; margin-top: -5px;" class="bi bi-circle-fill text-primary"></i> &nbsp; <?php echo $cap['status']; ?></span></p>
+                                        <?php } else if($cap['status'] == 'Interviewed') { ?>
+                                            <p><span class="float-end"><i style="font-size: 12px; margin-top: -5px;" class="bi bi-circle-fill text-info"></i> &nbsp; <?php echo $cap['status']; ?></span></p>
+                                        <?php } else if($cap['status'] == 'Offered') { ?>
+                                            <p><span class="float-end"><i style="font-size: 12px; margin-top: -5px;" class="bi bi-circle-fill text-success"></i> &nbsp; <?php echo $cap['status']; ?></span></p>
+                                        <?php } else if($cap['status'] == 'Rejected') { ?>
+                                            <p><span class="float-end"><i style="font-size: 12px; margin-top: -5px;" class="bi bi-circle-fill text-danger"></i> &nbsp; <?php echo $cap['status']; ?></span></p>
                                         <?php } ?>
                                     </div>
                                     <br>
