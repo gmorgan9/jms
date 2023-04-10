b348bcdeffa8457c1e7cd6154aebeccc9ba31911
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-04-09 19:35:39 -0600

    update

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ if (!isLoggedIN()) {
 </head>
 <body>
 
-<!-- <div class="header">
+<div class="header">
         <h3 class="logo ms-3 me-3">
             Job Management System
             <a class="float-end" href="index.php?logout='1'"><button style="cursor:pointer;" class="btn btn-link text-black"><i class="bi bi-box-arrow-left fs-5"></i></button></a>
@@ -44,11 +44,11 @@ if (!isLoggedIN()) {
     <a href="record-incident.php"><button class="btn btn-secondary rec">Record Job</button></a>
 </div>
 
-<?php // if (isset($_SESSION['fname'])) : ?>
-    	<h1 style="margin-left: 150px;" class="text-center">Welcome <strong><?php //echo $_SESSION['fname']; ?></strong></h1>
-    <?php //endif ?>
+<?php if (isset($_SESSION['fname'])) : ?>
+    	<h1 style="margin-left: 150px;" class="text-center">Welcome <strong><?php echo $_SESSION['fname']; ?></strong></h1>
+    <?php endif ?>
     <br>
- -->
+
 
 
 
@@ -58,7 +58,7 @@ if (!isLoggedIN()) {
  <div class="container-fluid main">
 
 <div class="row">
-  <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
+  <?php //include(ROOT_PATH . "/app/includes/header.php"); ?>
 </div>
 
 <div class="row">
