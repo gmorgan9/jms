0d9cd5472aa713c3d87270071c3228e22e0b69e3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-04-10 00:21:05 -0600

    update

--- create-app.php
+++ create-app.php
@@ -10,41 +10,6 @@ if (!isLoggedIN()) {
 }
 
 
-// ADD JOB
-if(isset($_POST['add-full'])){
-    $idno  = rand(1000000, 9999999);
-    $job_title = isset($_POST['job_title']) ? mysqli_real_escape_string($conn, $_POST['job_title']) : "NULL";
-    $company = isset($_POST['company']) ? mysqli_real_escape_string($conn, $_POST['company']) : "NULL";
-    $location = isset($_POST['location']) ? mysqli_real_escape_string($conn, $_POST['location']) : "NULL";
-    $job_desc = isset($_POST['job_desc']) ? mysqli_real_escape_string($conn, $_POST['job_desc']) : "NULL";
-	$pay = isset($_POST['pay']) ? mysqli_real_escape_string($conn, $_POST['pay']) : "NULL";
-    $bonus_pay = isset($_POST['bonus_pay']) ? mysqli_real_escape_string($conn, $_POST['bonus_pay']) : "NULL";
-    $status = isset($_POST['status']) ? mysqli_real_escape_string($conn, $_POST['status']) : "NULL";
-    $watchlist = isset($_POST['watchlist']) ? 1 : 0;
-	$app_link = isset($_POST['app_link']) ? mysqli_real_escape_string($conn, $_POST['app_link']) : "NULL";
-	$job_type = isset($_POST['job_type']) ? mysqli_real_escape_string($conn, $_POST['job_type']) : "NULL";
-	$contact_name = isset($_POST['contact_name']) ? mysqli_real_escape_string($conn, $_POST['contact_name']) : "NULL";
-	$contact_email = isset($_POST['contact_email']) ? mysqli_real_escape_string($conn, $_POST['contact_email']) : "NULL";
-	$contact_phone = isset($_POST['contact_phone']) ? mysqli_real_escape_string($conn, $_POST['contact_phone']) : "NULL";
-	$interview_set = isset($_POST['interview_set']) ? 1 : 0;
-	$start_date = isset($_POST['start_date']) ? mysqli_real_escape_string($conn, $_POST['start_date']) : "NULL";
-	$resume_used = isset($_POST['resume_used']) ? mysqli_real_escape_string($conn, $_POST['resume_used']) : "NULL";
-	$notes = isset($_POST['notes']) ? mysqli_real_escape_string($conn, $_POST['notes']) : "NULL";
-
-    $select1 = " SELECT * FROM applications WHERE idno = '$idno' ";
-    $result1 = mysqli_query($conn, $select1);
-
-    if(mysqli_num_rows($result1) > 0){
-      $error[] = 'Application already exist!';
-    }else{
-	  $insert1 = "INSERT INTO applications (idno, job_title, company, location, job_desc, pay, bonus_pay, status, watchlist, app_link, job_type, contact_name, contact_email, contact_phone, interview_set, start_date, resume_used, notes) VALUES ('$idno','$job_title','$company','$location','$job_desc','$pay','$bonus_pay','$status','$watchlist','$app_link','$job_type','$contact_name','$contact_email','$contact_phone','$interview_set','$start_date','$resume_used','$notes')";
-      mysqli_query($conn, $insert1);
-      header('location: /');
-    }
-};
-// END ADD JOB
-
-
 
 
 
@@ -93,36 +58,7 @@ if(isset($_POST['add-full'])){
 
 <br><br>
 
-<form action="" method="POST">
-            <div class="form-group">
-                <label for="job_title">Job Title:</label>
-                <input type="text" class="form-control" id="job_title" name="job_title">
-            </div>
-            <div class="form-group">
-                <label for="company">Company:</label>
-                <input type="text" class="form-control" id="company" name="company">
-            </div>
-            <div class="form-group">
-                <label for="location">Location:</label>
-                <input type="text" class="form-control" id="location" name="location">
-            </div>
-            <div class="form-group">
-                <label for="app_link">Application Link:</label>
-                <input type="text" class="form-control" id="app_link" name="app_link">
-            </div>
-            <br>
-            <div class="form-group">
-                <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="watchlist" name="watchlist" value="1">
-                    <label class="form-check-label" for="watchlist">Add to Watchlist</label>
-                </div>
-            </div>
-            <button type="submit" name="add-full" class="btn btn-primary">Submit</button>
-        </form>
-
- 
-
-    <!-- <form method="POST" action="">
+    <form method="POST" action="">
 
         <div class="row mb-3">
             <div class="col">
@@ -241,9 +177,9 @@ if(isset($_POST['add-full'])){
 
 
     
-        <button type="submit" name="add-full-job-app" class="btn btn-primary">Submit</button>
+        <button type="submit" name="add-full" class="btn btn-primary">Submit</button>
         <div class="pb-4"></div>
-    </form> -->
+    </form>
 
 
 
