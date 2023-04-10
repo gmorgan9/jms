13e877f07a527f07f9a2bc5ed6e40a2f99cf3871
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-04-09 22:24:37 -0600

    update

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ if (!isLoggedIN()) {
 </div>
 
 <?php if (isset($_SESSION['fname'])) : ?>
-    	<h1 style="margin-left: 125px;" class="text-center">Welcome <strong><?php echo $_SESSION['fname']; ?></strong></h1>
+    	<h1 style="margin-left: 175px;" class="text-center">Welcome <strong><?php echo $_SESSION['fname']; ?></strong></h1>
     <?php endif ?>
     <br>
 
