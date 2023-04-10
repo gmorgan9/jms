2c55f256d382112e3db9bf9a11c621b1cc9ac93b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-04-09 22:46:53 -0600

    update

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ if (!isLoggedIN()) {
     <div class="modal-dialog">
         <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title" id="quickModalLabel">Quick Modal</h5>
+            <h5 class="modal-title" id="quickModalLabel">Quick Application Creation</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
@@ -84,12 +84,14 @@ if (!isLoggedIN()) {
                 <label class="form-check-label" for="watchlist">Add to Watchlist</label>
                 </div>
             </div>
-            <button type="submit" name="add-quick-job-app" class="btn btn-primary">Submit</button>
-            </form>
+            
+            
         </div>
         <div class="modal-footer">
+            <button type="submit" name="add-quick-job-app" class="btn btn-primary">Submit</button>
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
         </div>
+        </form>
         </div>
     </div>
     </div>
