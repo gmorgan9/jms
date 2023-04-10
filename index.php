d67ddae9b6b41f0ef56e0e2aa1f3decdf2a4e6e2
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-04-09 18:43:10 -0600

    update

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (!isLoggedIN()) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/main.css?v=1.61">
+    <link rel="stylesheet" href="assets/css/main.css?v=1.62">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
     <!-- Bootstrap Icons -->
@@ -33,10 +33,10 @@ if (!isLoggedIN()) {
 <body>
 
 <div class="header">
-        <h2 class="logo ms-3 me-3">
+        <h3 class="logo ms-3 me-3">
             Job Management System
             <a class="float-end" href="/logout.php"><i class="bi bi-box-arrow-left fs-5"></i></a>
-        </h2>
+        </h3>
     </div>
 <br>
 <div class="record_incident">
