182d67f36e263b30d43bc2bbe5fd176557a915c3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-04-09 20:35:57 -0600

    update

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (!isLoggedIN()) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/main.css?v=1.73">
+    <link rel="stylesheet" href="assets/css/main.css?v=1.74">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
     <!-- Bootstrap Icons -->
