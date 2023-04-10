8e8c9ec0a1bbe916462a2b72549e99afa614349b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-04-09 19:32:39 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -8,3 +8,8 @@
 <div class="record_incident float-end me-5">
     <a href="record-incident.php"><button class="btn btn-secondary rec">Record Job</button></a>
 </div>
+
+<?php  if (isset($_SESSION['fname'])) : ?>
+    	<h1 style="margin-left: 150px;" class="text-center">Welcome <strong><?php echo $_SESSION['fname']; ?></strong></h1>
+ <?php endif ?>
+<br>
