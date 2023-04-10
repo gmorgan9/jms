96fc1996326526626e9ebb28c757d8b32c9cbfac
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-04-09 20:35:13 -0600

    update

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if (!isLoggedIN()) {
 <body>
 
 <div class="header">
-        <h3 class="logo ms-3 me-3 mt-2">
+        <h3 class="logo ms-3 me-3 pt-2">
             Job Management System
             <a class="float-end" href="index.php?logout='1'"><button style="cursor:pointer;" class="btn btn-link text-black"><i class="bi bi-box-arrow-left fs-5"></i></button></a>
         </h3>
