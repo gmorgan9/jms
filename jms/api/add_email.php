f183492937708c32a47c1a1036759697a4fb5398
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-02-22 14:36:01 -0600

    update

--- jms/api/add_email.php
+++ jms/api/add_email.php
@@ -9,12 +9,14 @@ $idno  = rand(1000000, 9999999);
 $setZero = 0;
 
 
-$query1 = "SELECT app_id FROM applications WHERE company = '" . $data['phrase'] . "'";
-$app_id = mysqli_query($conn, $query1);
+
 
 
 if(isset($data)) {
-$query = "INSERT INTO email_application (idno, app_id, subject,from, link, status) VALUES ('$idno','$app_id', '".$data['Subject']."', '".$data['Sender']."', 'Unread')";
+    $query1 = "SELECT app_id FROM applications WHERE company = '" . $data['phrase'] . "'";
+    $app_id = mysqli_query($conn, $query1);
+
+$query = "INSERT INTO email_application (idno, app_id, subject, sender, status) VALUES ('$idno','$app_id', '".$data['Subject']."', '".$data['Sender']."', 'Unread')";
 $result = mysqli_query($conn, $query);
 }
 // END JSON entry from PS Script
