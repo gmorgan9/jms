9de36a565887e5ca862902b22af7a10d148f333b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-02-22 16:56:09 -0600

    update

--- jms/api/companies.php
+++ jms/api/companies.php
@@ -2,23 +2,23 @@
 
 require('../app/database/connection.php');
 
-// Fetch data from the database
-$query = "SELECT company FROM applications";
+// Fetch distinct companies from the database
+$query = "SELECT DISTINCT company FROM applications";
 $result = mysqli_query($conn, $query);
 
 // Check if there are any rows returned
 if (mysqli_num_rows($result) > 0) {
     // Fetch data and store in array
-    $applications = array();
+    $companies = array();
     while ($row = mysqli_fetch_assoc($result)) {
-        $applications[] = $row;
+        $companies[] = $row['company'];
     }
 
     // Set HTTP headers to indicate JSON content
     header('Content-Type: application/json');
 
     // Output JSON data
-    echo json_encode($applications);
+    echo json_encode($companies);
 } else {
     // No data found
     echo json_encode(array('message' => 'No applications found'));
