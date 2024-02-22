ec19a87e0070c495c61687334dd0ea9c84f04263
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-02-22 09:28:51 -0600

    update

--- jms/update-app.php
+++ jms/update-app.php
@@ -159,6 +159,15 @@ foreach ($files as $file) {
             </div>
         </div>
 
+        <script>
+            tinymce.init({
+  selector: 'textarea',  // change this value according to your HTML
+  plugins: 'a_tinymce_plugin',
+  a_plugin_option: true,
+  a_configuration_option: 400
+});
+        </script>
+
         <div class="row mb-3 ps-3">
             <div class="form-check">
                 <?php if($watchlist == 1) { ?>
