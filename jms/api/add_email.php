e52d2b406c441d41216e7b210aa548fb88a85d2a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-02-22 16:04:59 -0600

    update

--- jms/api/add_email.php
+++ jms/api/add_email.php
@@ -2,39 +2,48 @@
 
 require('../app/database/connection.php');
 
+// JSON entry from PS Script
 $data = json_decode(file_get_contents('php://input'), true);
+
+$setZero = 0;
 
 if(isset($data)) {
     foreach ($data as $item) {
-        $phrase = mysqli_real_escape_string($conn, $item['phrase']);
-        $subject = mysqli_real_escape_string($conn, $item['subject']);
-        $sender = mysqli_real_escape_string($conn, $item['sender']);
-        $link = mysqli_real_escape_string($conn, $item['link']);
-
-        $query1 = "SELECT app_id FROM applications WHERE company = '$phrase'";
+        // Retrieve app_id from applications table
+        $query1 = "SELECT app_id FROM applications WHERE company = '" . mysqli_real_escape_string($conn, $item['phrase']) . "'";
         $result1 = mysqli_query($conn, $query1);
 
         if ($result1) {
-            $app_id = null;
             if (mysqli_num_rows($result1) > 0) {
+                // Fetch the app_id from the result set
                 $row = mysqli_fetch_assoc($result1);
                 $app_id = $row['app_id'];
+            } else {
+                // No matching company, set app_id to null
+                $app_id = null;
             }
 
-            $idno = rand(100000, 999999); // Generate unique ID
+            // Generate idno
+            $idno = rand(1000000, 9999999);
 
-            $query2 = "INSERT INTO email_application (idno, app_id, subject, sender) VALUES ('$idno', NULLIF('$app_id',''), NULLIF('$subject',''), NULLIF('$sender',''))";
+            // Insert data into email_application table
+            $query2 = "INSERT INTO email_application (idno, app_id, subject, sender) VALUES ('$idno', NULLIF('$app_id',''), '" . mysqli_real_escape_string($conn, $item['subject']) . "', '" . mysqli_real_escape_string($conn, $item['sender']) . "')";
             $result2 = mysqli_query($conn, $query2);
-// , NULLIF('$link','')
+
             if ($result2) {
+                // Insertion successful
                 echo "Data inserted successfully.";
             } else {
-                echo "Error inserting data.";
+                // Error inserting data
+                echo "Error inserting data: " . mysqli_error($conn);
             }
         } else {
-            echo "Error retrieving app_id.";
+            // Error retrieving app_id
+            echo "Error retrieving app_id: " . mysqli_error($conn);
         }
     }
 }
+
+// END JSON entry from PS Script
 
 ?>
