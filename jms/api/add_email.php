ca756387b97f9ca2d06954d5d89752fb15f15806
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-02-22 23:10:12 -0600

    update

--- jms/api/add_email.php
+++ jms/api/add_email.php
@@ -8,11 +8,14 @@ require('../app/database/connection.php');
 // JSON entry from PS Script
 $data = json_decode(file_get_contents('php://input'), true);
 
-$setZero = 0;
+if(isset($data['companyName'], $data['link'], $data['subject'], $data['sender'])) {
+    $companyName = mysqli_real_escape_string($conn, $data['companyName']);
+    $link = mysqli_real_escape_string($conn, $data['link']);
+    $subject = mysqli_real_escape_string($conn, $data['subject']);
+    $sender = mysqli_real_escape_string($conn, $data['sender']);
 
-if(isset($data)) {
     // Retrieve app_id from applications table
-    $query1 = "SELECT app_id FROM applications WHERE company = '" . mysqli_real_escape_string($conn, $data['companyName']) . "'";
+    $query1 = "SELECT app_id FROM applications WHERE company = '$companyName'";
     $result1 = mysqli_query($conn, $query1);
     
     if ($result1) {
@@ -27,28 +30,33 @@ if(isset($data)) {
         
         // Generate idno
         $idno = rand(1000000, 9999999);
-        $link = mysqli_real_escape_string($conn, $data['link']);
-        $query_check = "SELECT idno FROM email_application WHERE link = '$link' LIMIT 1";
-            $result_check = mysqli_query($conn, $query_check);
-            if (mysqli_num_rows($result_check) == 0) {
-        // Insert data into email_application table
-        $query2 = "INSERT INTO email_application (idno, app_id, subject, sender, link) VALUES ('$idno', NULLIF('$app_id',''), '" . mysqli_real_escape_string($conn, $data['subject']) . "', '" . mysqli_real_escape_string($conn, $data['sender']) . "', '" . mysqli_real_escape_string($conn, $data['link']) . "')";
-        $result2 = mysqli_query($conn, $query2);
         
-        if ($result2) {
-            // Insertion successful
-            echo "Data inserted successfully.";
+        // Check if the link already exists
+        $query_check = "SELECT idno FROM email_application WHERE link = '$link' LIMIT 1";
+        $result_check = mysqli_query($conn, $query_check);
+        
+        if (mysqli_num_rows($result_check) == 0) {
+            // Insert data into email_application table
+            $query2 = "INSERT INTO email_application (idno, app_id, subject, sender, link) VALUES ('$idno', NULLIF('$app_id',''), '$subject', '$sender', '$link')";
+            $result2 = mysqli_query($conn, $query2);
+            
+            if ($result2) {
+                // Insertion successful
+                echo "Data inserted successfully.";
+            } else {
+                // Error inserting data
+                echo "Error inserting data: " . mysqli_error($conn);
+            }
         } else {
-            // Error inserting data
-            echo "Error inserting data: " . mysqli_error($conn);
+            echo "Link already exists.";
         }
     } else {
         // Error retrieving app_id
         echo "Error retrieving app_id: " . mysqli_error($conn);
     }
-}
+} else {
+    echo "Missing required data.";
 }
 
 // END JSON entry from PS Script
-
 ?>
